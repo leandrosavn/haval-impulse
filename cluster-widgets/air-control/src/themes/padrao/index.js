e12b7d2741d90eb6cca0856cdc8213ae4fafe500
netseek <marcel.abrantes@gmail.com>
2026-03-22 02:51:19 -0300

    Fix: Re-consolidate CSS safely using string appending to maintain exact layout structures

---
+++ cluster-widgets/air-control/src/themes/padrao/index.js
@@ -0,0 +1,30 @@
+import nightStyle from 'url:./styles/night.style.css';
+import lightStyle from 'url:./styles/light.style.css';
+import mainMenuStyle from 'url:./styles/main.menu.css';
+
+import { createDashboardInfo } from '../../components/dashboardInfo.js';
+import { createAcControlScreen } from '../../components/aircon/mainAcControl.js';
+import { createRegenScreen } from '../../components/regen/regenControl.js';
+import { createDisplaySelectionScreen } from '../../components/display/themeSelection.js';
+import { createMainMenu } from '../../components/mainMenu.js';
+import { createMask } from '../../components/display/mask.js';
+import { createGraphScreen } from '../../components/graphs/graphs.js';
+
+export { nightStyle, lightStyle, mainMenuStyle };
+
+export const theme = {
+    name: 'padrao',
+    styles: [
+        nightStyle,
+        mainMenuStyle
+    ],
+    components: {
+        mask: createMask,
+        dashboardInfo: createDashboardInfo,
+        aircon: createAcControlScreen,
+        regen: createRegenScreen,
+        displaySelection: createDisplaySelectionScreen,
+        mainMenu: createMainMenu,
+        graph: createGraphScreen
+    }
+};
